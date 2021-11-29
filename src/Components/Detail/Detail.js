4583b650683197053ad241cd1f1317f4d4e0d4e5
yeonhwi98 <dusgnl485@gmail.com>
2021-11-29 12:13:17 +0900

    deploy 4

--- src/Components/Detail/Detail.js
+++ src/Components/Detail/Detail.js
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useState } from "react/cjs/react.development";
+
 import styled from "styled-components";
 import { movieApi } from "../../api";
 import { NotFound } from "../../NotFound";
